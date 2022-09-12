4515e01d59a6651099436b1dbf6494f168dedd1d
mariannem <mariannemunizat@gmail.com>
2022-09-12 19:50:24 -0300

    Pesquisas

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -23,8 +23,12 @@ export class TemaService {
    return this.http.get<Tema[]>('http://localhost:8080/tema', this.token)
   }
 
-  getByIdTema( id: number): Observable<Tema>{
-    return this.http.get<Tema>( ` http://localhost:8080/tema/${id}`, this.token)
+  getByIdTema(id: number): Observable<Tema>{
+    return this.http.get<Tema>(`http://localhost:8080/tema/${id}`, this.token)
+  }
+
+  getByNomeTema(nome: string): Observable<Tema[]>{
+    return this.http.get<Tema[]>(`http://localhost:8080/tema/nome/${nome}`, this.token)
   }
 
   postTema(tema: Tema): Observable<Tema>{
